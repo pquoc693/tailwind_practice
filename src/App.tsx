ddf3e5dd7da5ff4e5c008a66952943a4ece9a17c
pquoc693 <quoc.pv1999@aiaracorp.com>
2024-04-14 15:12:57 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="content-wrapper">
+      <header>header</header>
+      <main>
+        <div className="slider">slider</div>
+        <div className="story">story</div>
+        <div className="featured-mugs">featured-mugs</div>
+        <div className="more-products">more-products</div>
+        <div className="coffee-magazine">coffee-magazine</div>
+        <div className="lifestyle-stories">lifestyle-stories</div>
+        <div className="subscribe-us">subscribe-us</div>
+      </main>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
