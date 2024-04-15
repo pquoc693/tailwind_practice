76eeee3c597d0fcd3853105e25d676343550ef7f
pquoc693 <quoc.pv1999@aiaracorp.com>
2024-04-15 22:22:09 +0700

    header + header responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"]
+      },
+      colors: {
+        "light-coffee": "#c89f94"
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" }
+        }
+      },
+      animation: {
+        slideDown: "slideDown 0.4s ease-in-out"
+      }
+    }
   },
   plugins: []
 };
